73ab5f64989b1d4244762289094a5bd1fe1f98ba
jeanmarielepioufle <jm@nexmodeling.com>
2016-10-25 15:27:35 +0200

    enabled to save the init state

--- R/init.UH.R
+++ R/init.UH.R
@@ -4,8 +4,8 @@
 #' - The UH of the river,
 #' - the UH of the layers
 #' - The UH of the Mean Annual Discharge (MAD)
-#' @param method method for the initialization, "load", "source", "processed"
-#' @param path directory where to get the files, in used when method is "load" or "source"
+#' @param method method for the initialization, "load", "processed"
+#' @param path directory where to get the files, in used when method is "load"
 #' @param Timeresinsec time resolution of the process in second (1hour: 3600s, ... etc)
 #' @param modelLayer list of parameters about the Layers
 #'  list(maxL,speed,nbStepsDelay,z,distr,param, NoL)
@@ -13,6 +13,8 @@
 #'  list(maxL,speed,nbStepsDelay,z,distr,param)
 #' @param modelMAD list of parameters about the Mean Annual Discharge
 #'  list(maxL,speed,nbStepsDelay,z,distr,param)
+#' @param SAVE Save the results, Boolean
+#' @param pathResults Path of the results. By default: $HOME
 #' @keywords UH
 #' @export
 #' @examples
@@ -20,30 +22,30 @@
 #' init.UH()
 #' }
 
-init.UH <-function(method=NULL,path=NULL,Timeresinsec,modelLayer,modelRiver,modelMAD){
+init.UH <-function(method=NULL,path=NULL,Timeresinsec,modelLayer,modelRiver,modelMAD,SAVE=FALSE,pathResults="~/"){
 
 
-   UH <- switch(method,
-    "processed"    = init.processed(Timeresinsec=Timeresinsec,modelLayer=modelLayer,modelRiver=modelRiver,modelMAD=modelMAD),
-    "load"         = init.load(path=path),
-    "source"       = init.source(path=path),
+  UH <- switch(method,
+    "processed"    = init.processed(Timeresinsec=Timeresinsec,modelLayer=modelLayer,modelRiver=modelRiver,modelMAD=modelMAD,SAVE=SAVE,pathResults=pathResults),
+    "load"         = init.load(path=path,SAVE=SAVE,pathResults=pathResults),
     (message=paste0("Invalid method:", method,".")))
 
   return(UH)
 }
 
-init.load <- function(path){
+
+init.load <- function(path,SAVE,pathResults){
   load(paste0(path,"UH.rda"))
+  if (SAVE){
+    pathInit <- paste0(pathResults,"init/")
+    dir.create(pathInit, showWarnings = FALSE)
+    do.call("save", list(obj="UH", file=paste0(pathInit,"UH.rda")))
+  }
   return(UH)
 }
 
-init.source <- function(path){
-  source(paste0(path,"UH.R"),local=TRUE)
-  return(UH)
-}
 
-
-init.processed <- function(Timeresinsec,modelLayer,modelRiver,modelMAD) {
+init.processed <- function(Timeresinsec,modelLayer,modelRiver,modelMAD,SAVE,pathResults){
    if( (!is.null(Timeresinsec)) && (!is.null(modelLayer)) &&
        (!is.null(modelRiver)) && (!is.null(modelMAD)) ) {
 
@@ -78,6 +80,11 @@ init.processed <- function(Timeresinsec,modelLayer,modelRiver,modelMAD) {
 
      rm(res_UHriver,res_layerUH,res_UHMAD)
 
+     if (SAVE){
+       pathInit <- paste0(pathResults,"init/")
+       dir.create(pathInit, showWarnings = FALSE)
+       do.call("save", list(obj="UH", file=paste0(pathInit,"UH.rda")))
+     }
      return(res)
 
    } else stop("NULL arguments in init.processed UH")
